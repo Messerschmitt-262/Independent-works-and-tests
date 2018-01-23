ba9dfeb40ed732bd5a2f4c15d93b578ffdbd9574
Eduard.Zeleniuk <eduard.zeleniuk@gmail.com>
2018-01-23 09:47:11 +0200

    just one more commit

--- independent1var2.php
+++ independent1var2.php
@@ -1,33 +1,33 @@
 <?php
-	$table = 
-	[
-		['id' => 1, 'name' => 'Дима', 'age' => 23, 'salary' => 400], 
-		['id' => 2, 'name' => 'Петя', 'age' => 25, 'salary' => 500],
-		['id' => 3, 'name' => 'Вася', 'age' => 23, 'salary' => 500],
-		['id' => 4, 'name' => 'Коля', 'age' => 30, 'salary' => 1000],
-		['id' => 5, 'name' => 'Иван', 'age' => 27, 'salary' => 500],
-		['id' => 6, 'name' => 'Кирилл', 'age' => 28, 'salary' => 1000]
-	];
-	echo '<pre>';
-	var_dump($table);
-	echo '</pre>';
-
+//Исходная строка.
+$data = 'id:name:age
+1:Ваня:8
+2:Коля:10
+3:Саша:5';
+//Разбиение исходной строки $data на части по символу "\n".
+$strings = explode("\n", $data);
+//Перемещение из массива строк $strings в переменную $titles первой строки.
+$titles = array_shift($strings);
+//Разбиение строки $titles по символу ":" на отдельные слова (ключи).
+$keys = explode(":", $titles);
+foreach($strings as $string)
+{
+	$values = explode(":", $string);
+	foreach($values as $i => $value)
+	{
+		$key = $keys[$i];
+		$merged_array = 
+		[
+			$key => $value
+		];
+		echo '<pre>';
+		// echo 'values ';
+		var_dump($merged_array);
+		echo '</pre>';
+		// echo '<pre>';
+		// // echo 'values ';
+		// var_dump($key, $value);
+		// echo '</pre>';
+	}
+}
 ?>
-<!-- <?=$table[0][0]?> -->
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Independent work 1 variant 2</title>
-</head>
-<body>
-	<table>
-		<tr>
-			<th></th>
-		</tr>
-		<tr>
-			<td></td>
-		</tr>
-	</table>
-</body>
-</html>
