67517b5f2668c9e397e17491d72dd224f3275d63
Eduard.Zeleniuk <eduard.zeleniuk@gmail.com>
2018-01-23 09:43:57 +0200

    just simple commit

--- independent1.php
+++ independent1.php
@@ -1,40 +1,33 @@
 <?php
+//Исходная строка.
 $data = 'id:name:age
 1:Ваня:8
 2:Коля:10
 3:Саша:5';
-
+//Разбиение исходной строки $data на части по символу "\n".
 $strings = explode("\n", $data);
+//Перемещение из массива строк $strings в переменную $titles первой строки.
 $titles = array_shift($strings);
+//Разбиение строки $titles по символу ":" на отдельные слова (ключи).
 $keys = explode(":", $titles);
-
-// echo '<pre>';
-// echo 'strings ';
-// var_dump($strings);
-// echo '</pre>';
-
-// echo '<pre>';
-// echo 'strings ';
-// var_dump($titles);
-// echo '</pre>';
-
-// echo '<pre>';
-// echo 'keys ';
-// var_dump($keys);
-// echo '</pre>';
-
-foreach($strings as $string){
+foreach($strings as $string)
+{
 	$values = explode(":", $string);
-	foreach($values as $i => $value){
+	foreach($values as $i => $value)
+	{
 		$key = $keys[$i];
+		$merged_array = 
+		[
+			$key => $value
+		];
 		echo '<pre>';
-		echo 'values ';
-		var_dump($key, $value);
+		// echo 'values ';
+		var_dump($merged_array);
 		echo '</pre>';
+		// echo '<pre>';
+		// // echo 'values ';
+		// var_dump($key, $value);
+		// echo '</pre>';
 	}
-	// echo '<pre>';
-	// echo 'values ';
-	// var_dump($values);
-	// echo '</pre>';
 }
 ?>
