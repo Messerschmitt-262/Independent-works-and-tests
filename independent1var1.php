20f03de63fee70d6483b36521045a288098e820f
Your Name <detonator48@gmail.com>
2018-01-24 16:32:21 +0200

    Independent work 1 variant 1 done

--- independent1var1.php
+++ independent1var1.php
@@ -1,5 +1,5 @@
 <?php
-	$table = 
+	$staff = 
 	[
 		['id' => 1, 'name' => 'Дима', 'age' => 23, 'salary' => 400], 
 		['id' => 2, 'name' => 'Петя', 'age' => 25, 'salary' => 500],
@@ -8,28 +8,32 @@
 		['id' => 5, 'name' => 'Иван', 'age' => 27, 'salary' => 500],
 		['id' => 6, 'name' => 'Кирилл', 'age' => 28, 'salary' => 1000]
 	];
+	$keys_names = array_keys($staff[0]);
 	// echo '<pre>';
-	// var_dump($table);
+	// var_dump($keys_names);
 	// echo '</pre>';
-
 ?>
-<!-- <?=$table[0][0]?> -->
 <!doctype html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>Independent work 1 variant 2</title>
+  <title>Independent work 1 variant 1</title>
 </head>
 <body>
-	<table>
+	<table border="1">
 		<tr>
-		<?php foreach($table as $item): ?>
-			<th><?=$item['id']?></th>
-		<?php endforeach; ?>
+			<?php foreach($keys_names as $key_name): ?>
+				<th><?=$key_name?></th>
+			<?php endforeach; ?>	
 		</tr>
+			<?php foreach($staff as $person): ?>
+				
 		<tr>
-			<td></td>
+			<?php foreach($person as $item): ?>
+				<td><?=$item?></td>
+			<?php endforeach; ?>	
 		</tr>
+			<?php endforeach; ?>
 	</table>
 </body>
 </html>
